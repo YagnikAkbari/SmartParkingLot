71808565989e0babd56efb2cf4a76c40c668cd71
YagnikAkbari2003 <yagnik.a@medkart.in>
2026-07-26 19:44:18 +0530

    Design and develop parking lot system.

---
+++ src/test/java/com/example/smartparkinglot/SmartParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartparkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingLotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
